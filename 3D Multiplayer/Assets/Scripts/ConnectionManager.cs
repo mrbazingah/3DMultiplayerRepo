431cdfa72a583e210c7a182b139f83c0f6400470
mrbazingah <143615546+mrbazingah@users.noreply.github.com>
2026-09-12 18:24:51 +0200

    Local join and host

--- 3D Multiplayer/Assets/Scripts/ConnectionManager.cs
+++ 3D Multiplayer/Assets/Scripts/ConnectionManager.cs
@@ -1,14 +1,100 @@
+using System.Collections;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ConnectionManager : MonoBehaviour
 {
-    public void HostGame()
+    public static ConnectionManager Instance { get; private set; }
+
+    [SerializeField] string gameSceneName;
+    [SerializeField] string mainMenuSceneName;
+    [SerializeField] int maxConnections;
+
+    bool isConnecting;
+
+    void Awake()
     {
-        Debug.Log("Host Game");
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
     }
 
-    public void JoinGame()
+    void Start()
     {
-        Debug.Log("Join Game");
+        if (!NetworkManager.Singleton) { return; }
+
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+    }
+
+    public void HostGame()
+    {
+        if (isConnecting || !NetworkManager.Singleton) { return; }
+
+        isConnecting = true;
+        StartCoroutine(HostGameRoutine());
+    }
+
+    IEnumerator HostGameRoutine()
+    {
+        AsyncOperation load = SceneManager.LoadSceneAsync(gameSceneName);
+        yield return load;
+
+        bool hostStarted = NetworkManager.Singleton.StartHost();
+
+        if (!hostStarted)
+        {
+            Debug.LogError("Failed to start host.");
+            
+            isConnecting = false;
+            NetworkManager.Singleton.Shutdown();
+
+            SceneManager.LoadScene(mainMenuSceneName);
+
+            yield break;
+        }
+    }
+
+    public void JoinGame(string roomCode)
+    {
+        if (isConnecting || !NetworkManager.Singleton) { return; }
+
+        SceneManager.LoadScene(gameSceneName);
+
+        isConnecting = true;
+
+        bool clientStarted = NetworkManager.Singleton.StartClient();
+        if (!clientStarted)
+        {
+            Debug.LogError("Failed to start client.");
+
+            isConnecting = false;
+            NetworkManager.Singleton.Shutdown();
+
+            SceneManager.LoadScene(mainMenuSceneName);
+
+            return;
+        }
+    }
+
+    void OnClientConnected(ulong clientId)
+    {
+        if (clientId != NetworkManager.Singleton.LocalClientId) { return; }
+
+        isConnecting = false;
+    }
+
+    void OnDestroy()
+    {
+        if (!NetworkManager.Singleton) { return; }
+
+        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
     }
 }
