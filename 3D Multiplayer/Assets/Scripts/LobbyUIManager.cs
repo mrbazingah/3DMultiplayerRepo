83ba95e95df4f7130225658b3c82e545c809e162
mrbazingah <mrbazingah@gmail.com>
2026-09-14 21:06:32 +0200

    Start on lobby menu and settings

---
+++ 3D Multiplayer/Assets/Scripts/LobbyUIManager.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class LobbyUIManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI roomCodeText;
+
+    void Start()
+    {
+        // Get room code from connection manager
+    }
+}
